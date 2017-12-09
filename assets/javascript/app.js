ba564721b6e6c9f1a13ada4222c7992c6d92cbd3
jsalamat <salamat_jon@yahoo.com>
2017-12-08 23:58:59 -0500

    check browser for web GL

---
+++ assets/javascript/app.js
@@ -0,0 +1,6 @@
+if (!Detector.webgl) Detector.addGetWebGLMessage();
+
+var composer, renderer, camera, scene1, torus1, torus2;
+
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;

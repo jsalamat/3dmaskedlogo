336d3e50ab64cf007cf98726c982e4ca799336b5
jsalamat <salamat_jon@yahoo.com>
2017-12-09 23:18:48 -0500

    call init funtion and event listener and check mousemovement

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,3 +4,12 @@ var composer, renderer, camera, scene1, torus1, torus2;
 
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
+
+var mouseX = 0,
+    mouseY = 0,
+    cubes = [];
+var clock = new THREE.Clock();
+
+init();
+
+document.addEventListener('mousemove', onDocumentMouseMove, false);
